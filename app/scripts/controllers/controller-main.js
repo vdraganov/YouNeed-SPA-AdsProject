aa1fb5a28fef7a8ab4d345d8e8193314ae3c6ffd
vdraganov <vdraganov@hotmail.co.uk>
2015-01-03 18:50:34 +0000

    I gave up on writing my own css, will use bootstrap spacelab theme! And the listing of the categories and towns was done in 5 min -.- So much time lost (facepalm)

--- app/scripts/controllers/controller-main.js
+++ app/scripts/controllers/controller-main.js
@@ -2,4 +2,10 @@ AdsApp.controller('YouNeedAppController', function ($scope, mainData) {
 	mainData.getAllAds(function (resp) {
 		$scope.data = resp;
 	});
+	mainData.getAllTowns(function (resp) {
+		$scope.towns = resp;
+	});
+	mainData.getAllCategories(function (resp) {
+		$scope.categories = resp;
+	});
 })
--- app/scripts/services/mainPage.js
+++ app/scripts/services/mainPage.js
@@ -1,8 +1,25 @@
 AdsApp.factory('mainData', function ($http, $log) {
 	return {
-		// getAllTowns: ,
+		getAllTowns: function (success) {
+			$http({method: 'GET', url:'http://softuni-ads.azurewebsites.net/api/towns'})
+			.success(function (data, status, headers, config) {
+				success(data);
+			})
+			.error(function (data, status, headers, config) {
+				$log.warn(data);
+			})
+		},
 		getAllAds: function (success) {
 			$http({method: 'GET', url:'http://softuni-ads.azurewebsites.net/api/ads?PageSize=5&StartPage=2'})
+			.success(function (data, status, headers, config) {
+				success(data);
+			})
+			.error(function (data, status, headers, config) {
+				$log.warn(data);
+			})
+		},
+		getAllCategories: function (success) {
+			$http({method: 'GET', url:'http://softuni-ads.azurewebsites.net/api/categories'})
 			.success(function (data, status, headers, config) {
 				success(data);
 			})
