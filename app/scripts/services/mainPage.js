c7bc300520ea16ecda9e621a0d46176c0d9f0350
vdraganov <vdraganov@hotmail.co.uk>
2015-01-03 16:44:14 +0000

    all-ads listing done NEXT: visualising the content of the ads

--- app/scripts/services/mainPage.js
+++ app/scripts/services/mainPage.js
@@ -1,15 +1,15 @@
 AdsApp.factory('mainData', function ($http, $log) {
 	return {
-		getAllTowns: ,
+		// getAllTowns: ,
 		getAllAds: function (success) {
-			$http({method: 'GET', url:'http://softuni-ads.azurewebsites.net/api/ads?PageSize=5&StartPage=1'})
+			$http({method: 'GET', url:'http://softuni-ads.azurewebsites.net/api/ads?PageSize=5&StartPage=2'})
 			.success(function (data, status, headers, config) {
 				success(data);
 			})
 			.error(function (data, status, headers, config) {
 				$log.warn(data);
 			})
-		},
+		}
 
 	}
 })
